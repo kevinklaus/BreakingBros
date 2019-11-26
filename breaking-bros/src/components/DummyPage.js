653f689132d9dade90ecc35db0024a6694d5f24e
Kevin Klaus <kev.k1995@gmail.com>
2019-11-26 17:31:38 +0100

    added article dummy content

--- breaking-bros/src/components/DummyPage.js
+++ breaking-bros/src/components/DummyPage.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import '../App.css'
+import Grid from '@material-ui/core/Grid'
+
 
 function DummyPage() {
   return (
     <div>
-        <p>
-            Learn React tomorrow!
-        </p>
+       <div className="hero-image">
+            <div className="hero-text">
+                <h1>Women in Tech</h1>
+                <h4>These are the smart Women, People of Color and queer idols in technology!</h4>
+            </div>
+        </div> 
+        <Grid container justify="center" spacing={5}>
+            <Grid item xs={3}>
+                <h2>Women are cool and smart!</h2>
+            </Grid>
+            <Grid item xs={3}>
+                <h2>Queers can do tech!</h2>
+            </Grid>
+        </Grid>
+        <Grid container justify="center" spacing={5}>
+            <Grid item xs={3}>
+                <h2>Meet Evelynn, she invented the internet!</h2>
+            </Grid>
+            <Grid item xs={3}>
+                <h2>Diversity rocks!</h2>
+            </Grid>
+        </Grid>
     </div>
   );
 }
