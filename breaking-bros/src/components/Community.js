d8919d122514464d5a54c0569ee0f778e1980ef1
FernandoLoera <58215836+FernandoLoera@users.noreply.github.com>
2019-11-26 17:28:10 +0100

    add map

---
+++ breaking-bros/src/components/Community.js
@@ -0,0 +1,42 @@
+// @flow
+
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+
+type State = {
+  lat: number,
+  lng: number,
+  zoom: number,
+}
+const divStyle = {
+  height: '400 px'
+}
+export default class SimpleExample extends Component<{}, State> {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 16,
+    
+  }
+
+
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <div style={divStyle}>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+      </div>
+    )
+  }
+}
