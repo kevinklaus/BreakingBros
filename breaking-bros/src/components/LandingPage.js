3d5c6e949ac5a4e92cc33fdd888ff651a94a21a7
Kevin Klaus <kev.k1995@gmail.com>
2019-11-26 17:31:54 +0100

    added content to landing page

--- breaking-bros/src/components/LandingPage.js
+++ breaking-bros/src/components/LandingPage.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import hero from '../img/hero.jpg'
+import React from 'react'
+import {RouteButton} from './RouteButton'
+import '../App.css'
+import Grid from '@material-ui/core/Grid'
 
 function LandingPage() {
   return (
     <div>
-        <h1>Women in Tech</h1>
-        <img src={hero} alt="Woman working on computer"/>
-
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>Women in Tech</h1>
+                <p>get inspired by the smart Women, PoC and queer idols in tech</p>
+                <RouteButton color="primary" variant="outlined" text="Read article" route="/dummy" color="inherit"/>
+            </div>
+        </div> 
+        <Grid container justify="center">
+            <Grid item xs={8}>
+                HI
+            </Grid>
+        </Grid>
+        <Grid container justify="center">
+            <Grid item xs={8}>
+                HI
+            </Grid>
+        </Grid>
     </div>
   );
 }
